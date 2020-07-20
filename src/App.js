c8a12e2463190a906216467590e1604e28630af8
user <mattjackmcgregor@gmail.com>
2020-07-20 18:38:10 +1200

    created header component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+
+import './App.css'
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Kiaora world</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <Header />
+        <div className='App-content'>
+          <Switch>
+            <Route path='/checkout'>
+              <h1>checkout</h1>
+            </Route>
+            <Route path='/login'>
+              <h1>login</h1>
+            </Route>
+            <Route path='/'>
+            <h1>home</h1>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
